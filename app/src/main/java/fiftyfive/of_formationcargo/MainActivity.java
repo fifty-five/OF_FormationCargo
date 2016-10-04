33ce81c1310fe5e8d603c5698ca726253f35b9f4
fhkhoury <f.khouryat@gmail.com>
2016-10-04 16:13:52 +0200

    creation du projet dans Android Studio

---
+++ app/src/main/java/fiftyfive/of_formationcargo/MainActivity.java
@@ -0,0 +1,13 @@
+package fiftyfive.of_formationcargo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
